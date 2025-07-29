0c273260126edf0fd78ad16db43b1ea21cf37f7d
AEnyChris <enyonejoseph@gmail.com>
2025-07-30 00:23:57 +0100

    created folder task_0, installed webpack & webpack-cli, created entry file for webpack

---
+++ task_0/src/index.js
@@ -0,0 +1,9 @@
+const $ = require('jquery');
+
+$(document).ready(function () {
+  $('body')
+    .append('<p>ALX Dashboard</p>')
+    .append('<p>Dashboard data for the students</p>')
+    .append('<p>Copyright - ALX</p>');
+});
+
